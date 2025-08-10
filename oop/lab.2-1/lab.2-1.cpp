f96b03a3fcfef41af52fcaca02a301715058392a
MurLuvAmur <katguseva03@gmail.com>
2025-08-10 15:45:47 +0300

    add lab.2

---
+++ oop/lab.2-1/lab.2-1.cpp
@@ -0,0 +1,34 @@
+﻿#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Открываем файл для чтения
+    ifstream in("file.txt");
+    if (!in) {
+        cerr << "Не удалось открыть файл для чтения." << endl;
+        return 1;
+    }
+
+    double num, sum = 0.0;
+
+    // Считываем числа и суммируем их
+    while (in >> num) {
+        sum += num;
+    }
+
+    in.close();
+
+    // Открываем файл для добавления суммы в конец
+    ofstream out("file.txt", ios::app);
+    if (!out) {
+        cerr << "Не удалось открыть файл для записи." << endl;
+        return 1;
+    }
+
+    // Записываем сумму на новой строке
+    out << endl << sum;
+    out.close();
+
+    return 0;
+}
---
+++ oop/lab.2-2/lab.2-2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    string filename;
+    int N;
+
+    // Ввод имени файла и числа N
+    cout << "Введите имя файла: ";
+    cin >> filename;
+    cout << "Введите N (>1): ";
+    cin >> N;
+
+    // Проверка корректности N
+    if (N <= 1) {
+        cerr << "Ошибка: N должно быть больше 1!" << endl;
+        return 1;
+    }
+
+    // Создание и открытие файла
+    ofstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Ошибка создания файла!" << endl;
+        return 1;
+    }
+
+    // Запись чисел в файл
+    for (int i = 1; i <= N; ++i) {
+        int num = 2 * i;
+        file << num;
+        if (i != N) file << " "; // Добавляем пробел между числами
+    }
+
+    file.close();
+    cout << "Файл успешно создан!" << endl;
+    return 0;
+}
