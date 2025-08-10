35bcaef69151a27174fee071de9477776bbbf6e6
MurLuvAmur <katguseva03@gmail.com>
2025-08-10 15:47:36 +0300

    add lab 3

---
+++ oop/lab.3-1/lab.3-1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    string input;
+    string vowels = "aeiouyAEIOUY";
+    string result;
+
+    // Ввод строки
+    cout << "Введите строку: ";
+    getline(cin, input);
+
+    // Фильтрация гласных
+    for (char c : input) {
+        if (vowels.find(c) == string::npos) {
+            result += c;
+        }
+    }
+
+    // Вывод результата
+    cout << "Результат: " << result << endl;
+
+    return 0;
+}
---
+++ oop/lab.3-2/lab.3-2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+    ifstream file("file.txt");
+    vector<string> sentences;
+    string line;
+
+    if (!file.is_open()) {
+        cerr << "error" << endl;
+        return 1;
+    }
+
+    while (getline(file, line)) {
+        sentences.push_back(line);
+        if (sentences.size() >= 3) break; // Читаем не более трёх строк
+    }
+
+    file.close();
+
+    cout << "Результат:\n";
+    // Выводим строки в обратном порядке, но только существующие
+    for (int i = sentences.size() - 1; i >= 0; --i) {
+        cout << sentences[i] << endl;
+    }
+
+    return 0;
+}
